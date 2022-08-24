c6695384c62b025118e302cb6662916d30f8b824
Colby Pate <cbpate08@gmail.com>
2022-08-24 15:03:38 -0500

    Final Commit

---
+++ ClassesExecsie1/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string makeInput, string modelInput, int yearInput)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = yearInput;
+        }
+
+        public string Make;
+        public string Model;
+        public int Year;
+    }
+}
---
+++ ClassesExecsie1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "4-Runner";
+            myCar.Year = 2000;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, mazda, chevy };
+
+            foreach( var car in carList )
+            {
+                Console.WriteLine($"Make: {car.Make}");
+                Console.WriteLine($"Model: {car.Model}");
+                Console.WriteLine($"Year: {car.Year}");
+            }
+        }
+    }
+}
